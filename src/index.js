0cd696c65c2f9dcd6ad376a658b7015b3d3eb6c2
Deepak Verma <85281412+Deepakv14@users.noreply.github.com>
2022-02-23 00:48:18 +0530

    Backend files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+const socketIO = require('socket.io');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const port = process.env.PORT;
+app.use(cors()); //CORS is used for Inter URl communication
+app.get('/', (req, res) => {
+    res.send("We are gonna create a Real time chat app!");
+});
+const users = [{}];
+io.on('connection', (socket) => {
+    console.log("New Connection");
+    socket.on('joined', ({ user }) => {
+        users[socket.id] = user;
+        console.log(`${user} has joined!`);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        socket.emit('welcome', { user: "Admin", message: `Welcome to the chat, ${users[socket.id]}!` });
+        socket.broadcast.emit('userJoined', { user: "Admin", message: `${users[socket.id]} has joined` });
+    })
+    socket.on('message', ({ message, id }) => {
+        // socket.broadcast.emit --> to everyone except user
+        // socket.emit --> to user only
+        io.emit('sendMessage', { user: users[id], message, id })
+
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('leave', { user: "Admin", message: `${users[socket.id]} left the chat!` });
+    })
+})
+server.listen(port, () => {
+    console.log(`server is working on http://localhost:${port}`);
+})
